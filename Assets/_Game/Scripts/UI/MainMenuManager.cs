7cb961bff19445fd8a2a70a0540203cc3825cf33
Mohammed Albalooshi <mohammedalbalooshi93@outlook.com>
2025-10-14 23:54:33 +0300

    Added Main Menu UI and transitions

---
+++ Assets/_Game/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private SceneFader fader;
+
+    public void StartGame()      => StartCoroutine(fader.FadeOut("Quiz"));
+    public void OpenAchievements()=> StartCoroutine(fader.FadeOut("Achievements"));
+    public void OpenSettings()   => StartCoroutine(fader.FadeOut("Settings"));
+    public void QuitGame()       => Application.Quit();
+}
